b27a3c9e534fea44330293d7a3594d683a07d1cf
Rafaela Perruci <rafaelaoperruci@gmail.com>
2023-08-01 10:46:25 -0300

    introdução à expressão Lambda

---
+++ lamda-introducao.py
@@ -0,0 +1,15 @@
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+l1 = sorted(lista, key=lambda item: item["nome"])
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+
+exibir(l1)
